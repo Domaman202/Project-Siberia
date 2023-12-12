b13ffa1d0fdb2daa039b5e84947b55061640a124
DomamaN202 <vip.domaman@mail.ru>
2023-12-12 09:49:45 +0700

    Maven Local Publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.21"
+    `maven-publish`
 }
 
 group = "ru.DmN.siberia"
@@ -24,20 +25,18 @@ java {
 }
 
 tasks {
-    val fatJar = register<Jar>("fatJar") {
-        dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources")) // We need this for Gradle optimization to work
-        archiveClassifier.set("standalone") // Naming the jar
-        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-        manifest { attributes(mapOf("Main-Class" to "ru.DmN.pht.base.ConsoleOld")) } // Provided we set it up in the application plugin configuration
-        val sourcesMain = sourceSets.main.get()
-        val contents = configurations.runtimeClasspath.get()
-            .map { if (it.isDirectory) it else zipTree(it) } +
-                sourcesMain.output
-        from(contents)
-    }
-
     build {
-        dependsOn(fatJar) // Trigger fat jar creation during build
+        dependsOn(register<Jar>("fatJar") {
+            dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources")) // We need this for Gradle optimization to work
+            archiveClassifier.set("standalone") // Naming the jar
+            duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+            manifest { attributes(mapOf("Main-Class" to "ru.DmN.pht.base.ConsoleOld")) } // Provided we set it up in the application plugin configuration
+            val sourcesMain = sourceSets.main.get()
+            val contents = configurations.runtimeClasspath.get()
+                .map { if (it.isDirectory) it else zipTree(it) } +
+                    sourcesMain.output
+            from(contents)
+        })
     }
 
     java {
@@ -46,5 +45,16 @@ tasks {
 
     test {
         useJUnitPlatform()
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group as String
+            artifactId = "Project-Siberia"
+            version = project.version as String
+            from(components["java"])
+        }
     }
 }
