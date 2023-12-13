9efac8a7d06d0beb23404266454584776a0f08fc
DomamaN202 <vip.domaman@mail.ru>
2023-12-13 07:21:40 +0700

    Gradle fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,18 +25,18 @@ java {
 }
 
 tasks {
+    val fatJar = register<Jar>("fatJar") {
+        dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources"))
+        archiveClassifier.set("standalone")
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        manifest { attributes(mapOf("Main-Class" to "ru.DmN.pht.base.Console")) }
+        val sourcesMain = sourceSets.main.get()
+        val contents = configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) } + sourcesMain.output
+        from(contents)
+    }
+
     build {
-        dependsOn(register<Jar>("fatJar") {
-            dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources")) // We need this for Gradle optimization to work
-            archiveClassifier.set("standalone") // Naming the jar
-            duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-            manifest { attributes(mapOf("Main-Class" to "ru.DmN.pht.base.ConsoleOld")) } // Provided we set it up in the application plugin configuration
-            val sourcesMain = sourceSets.main.get()
-            val contents = configurations.runtimeClasspath.get()
-                .map { if (it.isDirectory) it else zipTree(it) } +
-                    sourcesMain.output
-            from(contents)
-        })
+        dependsOn(fatJar)
     }
 
     java {
