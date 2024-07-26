4651dd832a5ff7c42ed29e1581e7839c3da12d4f
DomamaN202 <vip.domaman@mail.ru>
2024-07-26 09:14:25 +0700

    Node print update

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "ru.DmN.siberia"
-version = "1.24.0"
+version = "1.24.1"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/ru/DmN/siberia/ast/INodesList.kt
+++ src/main/kotlin/ru/DmN/siberia/ast/INodesList.kt
@@ -1,5 +1,7 @@
 package ru.DmN.siberia.ast
 
+import ru.DmN.siberia.utils.indent
+
 /**
  * Интерфейс описывающий ноду, имеющую под-ноды.
  */
@@ -11,4 +13,34 @@ interface INodesList : Node {
 
     override fun copy(): INodesList =
         this
+
+    override fun print(builder: StringBuilder, indent: Int): StringBuilder =
+        printNodes(builder.indent(indent).append('[').append(info.type), indent).append(']')
+
+    override fun printShort(builder: StringBuilder, indent: Int): StringBuilder =
+        printNodesShort(builder.indent(indent).append('[').append(info.type), indent).append(']')
+
+    /**
+     * Печатает под-ноды в "builder".
+     */
+    fun printNodes(builder: StringBuilder, indent: Int): StringBuilder {
+        if (nodes.isNotEmpty())
+            builder.append('\n')
+        nodes.forEach { it.print(builder, indent + 1).append('\n') }
+        if (nodes.isNotEmpty())
+            builder.indent(indent)
+        return builder
+    }
+
+    /**
+     * Печатает под-ноды в "builder" в кратком варианте.
+     */
+    fun printNodesShort(builder: StringBuilder, indent: Int): StringBuilder {
+        if (nodes.isNotEmpty())
+            builder.append('\n')
+        nodes.forEach { it.printShort(builder, indent + 1).append('\n') }
+        if (nodes.isNotEmpty())
+            builder.indent(indent)
+        return builder
+    }
 }
